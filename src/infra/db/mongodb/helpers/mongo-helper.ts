85615a416b0dd86e768e71fa628a553482284c6f
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-20 19:53:33 -0300

    feat: ensure AccountMongoRepository returns an account on sucess

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
-import { MongoClient, MongoClientOptions } from 'mongodb';
-import { disconnect } from 'process';
+import { Collection, MongoClient, MongoClientOptions } from 'mongodb';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -13,5 +12,16 @@ export const MongoHelper = {
 
   async disconnect(): Promise<void> {
     await this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
+  },
+
+  map(collection: any): any {
+    // _id é desconstruído e armazenado numa variável homônima
+    // 'accountWithoutId' é um objetos com todos os atributos de 'accountById', exceto o _id, que virou variável
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id.toHexString() });
   },
 };
