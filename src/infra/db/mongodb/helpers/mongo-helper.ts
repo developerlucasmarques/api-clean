5f67b441cc202e63dda68c07496df52f4d868663
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-21 20:31:04 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -3,9 +3,11 @@ import { AccountModel } from '../../../../domain/models/account';
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
 
-  async connect(url: string): Promise<void> {
-    this.client = await MongoClient.connect(url, {
+  async connect(uri: string): Promise<void> {
+    this.uri = uri;
+    this.client = await MongoClient.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as MongoClientOptions);
@@ -13,15 +15,19 @@ export const MongoHelper = {
 
   async disconnect(): Promise<void> {
     await this.client.close();
+    this.client = null;
   },
 
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.uri);
+    }
     return this.client.db().collection(name);
   },
 
   map(collection: any): AccountModel {
     // _id é desconstruído e armazenado numa variável homônima
-    // 'accountWithoutId' é um objetos com todos os atributos de 'accountById', exceto o _id, que virou variável
+    // 'accountWithoutId' é um objeto com todos os atributos de 'accountById', exceto o _id, que virou variável
     const { _id, ...collectionWithoutId } = collection;
     return Object.assign({}, collectionWithoutId, { id: _id.toHexString() });
   },
