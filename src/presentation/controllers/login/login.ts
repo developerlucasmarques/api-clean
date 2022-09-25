dd13c417f3dc82bfc9de785e302c810578731212
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-25 17:24:11 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -5,7 +5,7 @@ import {
   ok,
   serverError,
   unauthorized,
-} from '../../helpers/http-helper';
+} from '../../helpers/http/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 import { Validation } from '../signup/signup-protocols';
 
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,4 +1,4 @@
-import { badRequest, created, serverError } from '../../helpers/http-helper';
+import { badRequest, created, serverError } from '../../helpers/http/http-helper';
 import {
   AddAccount,
   Controller,
