50f470cdedbd76226aef41c4f9114833874be1d2
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-09-26 15:23:03 -0300

    feat: ensure AccountMongoRepository calls loadByEmail whith correct email

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,12 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository';
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../../../../domain/models/account';
 import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository
+{
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
@@ -13,5 +16,11 @@ export class AccountMongoRepository implements AddAccountRepository {
       await accountCollection.findOne({ _id: id })
     );
     return account;
+  }
+
+  async loadByEmail(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    let account = await accountCollection.findOne({ email });
+    return MongoHelper.map(account);
   }
 }
