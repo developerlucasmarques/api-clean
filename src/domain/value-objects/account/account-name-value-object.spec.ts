01d5f3043f1cf072df8b54a8390912a02864920c
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-10-07 15:00:45 -0300

    test: ensure AccountName return left with InvalidNameError if name is not provided

--- src/domain/value-objects/account/account-name-value-object.spec.ts
+++ src/domain/value-objects/account/account-name-value-object.spec.ts
@@ -1,8 +1,11 @@
+import { left } from '../../either/either';
+import { InvalidNameError } from '../../errors/invalid-name';
 import { AccountName } from './account-name-value-objec';
 
 describe('AccountName', () => {
-  test('Should return left if name is not provided', () => {
+  test('Should return left with InvalidNameError if name is not provided', () => {
     const response = AccountName.create('');
-    expect(response.isLeft()).toBe(true);
+    const leftError = left(new InvalidNameError(`Account name not informed`));
+    expect(response.value).toEqual(leftError.value);
   });
 });
