2d199771381bc7fd67ea76f8d39466b5c5dd5068
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-10-07 15:04:01 -0300

    test: ensure AccountName return left with InvalidNameError if name is less than 2 characters

--- src/domain/value-objects/account/account-name-value-object.spec.ts
+++ src/domain/value-objects/account/account-name-value-object.spec.ts
@@ -8,4 +8,12 @@ describe('AccountName', () => {
     const leftError = left(new InvalidNameError(`Account name not informed`));
     expect(response.value).toEqual(leftError.value);
   });
+
+  test('Should return left with InvalidNameError if name is less than 2 characters', () => {
+    const response = AccountName.create('A');
+    const leftError = left(
+      new InvalidNameError(`Account name must contain between 2 and 100 characters`)
+    );
+    expect(response.value).toEqual(leftError.value);
+  });
 });
