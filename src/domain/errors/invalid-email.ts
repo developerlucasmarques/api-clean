396a00254f2339f7c0a5bfaffa5483095b2ab4ae
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-10-07 17:19:44 -0300

    feat: ensure AccountEmail return  left with InvalidEmailError if email not provided

---
+++ src/domain/errors/invalid-email.ts
@@ -0,0 +1,6 @@
+export class InvalidEmailError extends Error implements DomainError {
+  constructor(message: string) {
+    super(message);
+    this.name = 'InvalidEmailError';
+  }
+}
---
+++ src/domain/value-objects/account/account-email.ts
@@ -0,0 +1,21 @@
+import { Either, left, rigth } from '../../either/either';
+import { InvalidEmailError } from '../../errors/invalid-email';
+
+type AccountEmailResponse = Either<InvalidEmailError, AccountEmail>;
+
+export class AccountEmail {
+  private constructor(private readonly email: string) {
+    Object.freeze(this);
+  }
+
+  get value(): string {
+    return this.email;
+  }
+
+  public static create(email: string): AccountEmailResponse {
+    if (!email) {
+      return left(new InvalidEmailError(`Account email not informed`));
+    }
+    return rigth(new AccountEmail(email));
+  }
+}
