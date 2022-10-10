aef08b2a05b616ba0ac5357a5fcceca0079b221f
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-10-10 18:38:02 -0300

    test: ensure Account return account created if data provided is valid

--- src/domain/models/account/account.spec.ts
+++ src/domain/models/account/account.spec.ts
@@ -3,13 +3,27 @@ import { InvalidPasswordError } from '../../errors/invalid-password';
 import { Account } from './account';
 
 describe('Account', () => {
-  test('Should EitherComine return the first error if any field invalid', () => {
+  test('Should EitherCombine return the first error if any field invalid', () => {
     const response = Account.create({
       name: 'any_name',
       email: 'mail@mail.com',
-      password: 'any@assword',
+      password: 'any@password',
     });
     const leftError = left(new InvalidPasswordError('Password too weak'));
     expect(response).toEqual(leftError);
+  });
+
+  test('Should return new Account if data provided is valid', () => {
+    const response = Account.create({
+      name: 'valid_name',
+      email: 'valid@mail.com',
+      password: 'Valid@Password123',
+    });
+
+    expect(response.value).toEqual({
+      name: { name: 'valid_name' },
+      email: { email: 'valid@mail.com' },
+      password: { password: 'Valid@Password123' },
+    });
   });
 });
