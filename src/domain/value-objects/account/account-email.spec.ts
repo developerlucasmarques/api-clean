f525029e60d5a02d80064f08c83ae28f3980e437
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-10-10 09:55:32 -0300

    test: ensure AccountEmail return InvalidEmailError if email contains more than 100 characters

--- src/domain/value-objects/account/account-email.spec.ts
+++ src/domain/value-objects/account/account-email.spec.ts
@@ -9,6 +9,16 @@ describe('AccountEmail', () => {
     expect(response.value).toEqual(leftError.value);
   });
 
+  test('Should return left with InvalidEmailError if email contains more than 100 characters', () => {
+    const response = AccountEmail.create(
+      'any_email_any_email_any_email_any_email_any_email_any_email_any_email_any_email_any_email_any_email_1'
+    );
+    const leftError = left(
+      new InvalidEmailError(`Email cannot contain more than 100 characters`)
+    );
+    expect(response.value).toEqual(leftError.value);
+  });
+
   test('Should create email with no white spaces in edges', () => {
     const response = AccountEmail.create('  any_email@mail.com    ');
     expect(response.value).toEqual({ email: 'any_email@mail.com' });
