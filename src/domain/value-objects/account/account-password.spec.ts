2594363f9c3df96b690afff4d207b70b907d1af7
Lucas Marques <76230078+mlucasdev@users.noreply.github.com>
2022-10-10 08:37:52 -0300

    test: ensure return InvalidPasswordError if password is weak

--- src/domain/value-objects/account/account-password.spec.ts
+++ src/domain/value-objects/account/account-password.spec.ts
@@ -3,9 +3,15 @@ import { InvalidPasswordError } from '../../errors/invalid-password';
 import { AccountPassword } from './account-password';
 
 describe('AccountPassword', () => {
-  test('Should return left with InvalidPassword if password not informed', () => {
+  test('Should return left with InvalidPasswordError if password not informed', () => {
     const response = AccountPassword.create('');
     const leftError = left(new InvalidPasswordError('Password not informed'));
+    expect(response.value).toEqual(leftError.value);
+  });
+
+  test('Should return left with InvalidPasswordError if password is weak', () => {
+    const response = AccountPassword.create('any_password');
+    const leftError = left(new InvalidPasswordError('Password too weak'));
     expect(response.value).toEqual(leftError.value);
   });
 });
